5b5f1a62d2dca6a9db6348989a81c3167fbcb702
nabiursiddique <nabiursiddique01@gmail.com>
2022-10-12 14:09:58 +0530

    Now the quiz topic is showing on the quiz page

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './Quiz.css'
 
 const Quiz = ({ quiz, index }) => {
-    console.log(quiz)
+    // console.log(quiz)
     const { question, options, correctAnswer } = quiz;
-    console.log(correctAnswer)
+    // console.log(correctAnswer)
     return (
         <div className='container bg-dark text-white p-5 rounded pb-5 question-container border-bottom '>
             <p className=' h5'>{index + 1}. {question.replace(/(<([^>]+)>)/ig, '')}</p>
--- src/components/Quizes/Quizes.js
+++ src/components/Quizes/Quizes.js
@@ -5,10 +5,10 @@ import Quiz from '../Quiz/Quiz';
 const Quizes = () => {
     const quizData = useLoaderData();
     const quizD = quizData.data.questions;
-    // console.log(quizD);
+    const name = quizData.data.name;
     return (
         <div className='bg-black py-5'>
-            <h3 className='text-info text-center fw-bold pb-5'>QUIZ PAGE</h3>
+            <h3 className='text-info text-center fw-bold pb-5'>Topic: {name}</h3>
             {
                 quizD.map((quiz, index) => <Quiz
                     key={quiz.id}
