4e3150064083d27e534d3eae42aca8944136968a
nabiursiddique <nabiursiddique01@gmail.com>
2022-10-12 19:02:11 +0530

    rechart is added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const Statistics = () => {
+    const quizData = useLoaderData();
+    const totalArr = quizData.data;
+    console.log(totalArr)
     return (
-        <div>
-            <h2 className='text-center'>This is statistics page</h2>
+        <div >
+            <h2 className='text-center mt-5'>This Is Statistics Page</h2>
+            <div className='d-flex justify-content-center mt-5'>
+                <div>
+                    <LineChart width={500} height={400} data={totalArr}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+                    </LineChart>
+                </div>
+            </div>
         </div>
     );
 };
