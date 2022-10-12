6bb6b7c292c2dda207f937c5e4c284366610c3b5
nabiursiddique <nabiursiddique01@gmail.com>
2022-10-12 22:36:49 +0530

    Now the correct and worng answer is showing

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -5,12 +5,24 @@ const Quiz = ({ quiz, index }) => {
     // console.log(quiz)
     const { question, options, correctAnswer } = quiz;
     // console.log(correctAnswer)
+    const checkAns = (option) => {
+        if (option === correctAnswer) {
+            alert('correct answer');
+        } else {
+            alert('wrong answer');
+        }
+    }
     return (
         <div className='container bg-dark text-white p-5 rounded pb-5 question-container border-bottom '>
             <p className=' h5'>{index + 1}. {question.replace(/(<([^>]+)>)/ig, '')}</p>
 
 
-            <p>{options.map((option, ind) => <p> <input type="radio" name={`value${ind}`}></input> {option}</p>)}</p>
+            {/* <p>{options.map((option, ind) => <p><input type="radio" name={{ ind }}></input> {option}</p>)}</p> */}
+            <p>{options.map((option, ind) =>
+                <div className='hover-work' onClick={() => checkAns(option)}>
+                    <input type="radio" name={{ ind }}></input>{option}
+                </div>)}
+            </p>
         </div>
     );
 };
